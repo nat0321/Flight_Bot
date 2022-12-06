86e7c421ecde2fa24fad4c37abdaf2f21f039c4a
nat0321 <94549811+nat0321@users.noreply.github.com>
2022-12-05 19:20:05 -0600

    V1 Config File

---
+++ config.py
@@ -0,0 +1,8 @@
+# Flight Bot Config
+
+# Discord Bot Token
+TOKEN = "token" #Insert bot token here
+
+# ADS-B Data
+# Change this to location of your aircraft.json file
+adsb_url = "http://192.168.2.30/tar1090/data/aircraft.json"
